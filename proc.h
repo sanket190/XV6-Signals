88457c2cd12fd7b4263d9a02af95c5daecc5c082
rushikesh <r4002001005025k@gmail.com>
2022-04-03 00:21:24 +0530

    modified struct proc

--- proc.h
+++ proc.h
@@ -49,6 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  uint pending_sigs[32];       // rusa: array of pending signals
+  uint sig_mask[32];         // rusa: array of signal mask
+  void* sig_handler[32];    // rusa: signal handlers
 };
 
 // Process memory is laid out contiguously, low addresses first:
